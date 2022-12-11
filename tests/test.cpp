1838bbef51f9aa798fdc6129f2877959eec17a15
schwenk4 <117411160+schwenk4@users.noreply.github.com>
2022-12-10 21:31:26 -0600

    Add files via upload

--- tests/test.cpp
+++ tests/test.cpp
@@ -318,11 +318,26 @@ void testSolveRoom() {
     // n o n
     // n n o
     // n n n
+    room.setWalkingDistance(3);
 
     std::vector<std::vector<int>> answr = room.solveRoom();
     for (unsigned i = 0; i < answr.size(); i++) {
         for(unsigned j = 0; j < answr[0].size(); j++) {
             std::cout <<"solution:" << answr[i][j] << std::endl;
+        }
+    }
+}
+void testSolveRoom2() {
+    Room room;
+    room.makeRoom(3,3);
+    room.setWalkingDistance(3);
+    cs225::PNG* unsolved = room.drawRoom();
+    unsolved->writeToFile("unsolved1.png");
+
+    std::vector<std::vector<int>> answr = room.solveRoom();
+    for (unsigned i = 0; i < answr.size(); i++) {
+        for(unsigned j = 0; j < answr[0].size(); j++) {
+            std::cout <<"solution:" <<answr[i][j] << std::endl;
         }
     }
 }
