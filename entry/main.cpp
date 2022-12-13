b3c29a831f2499293a975c0ac33b8162eb15e030
schwenk4 <117411160+schwenk4@users.noreply.github.com>
2022-12-12 18:10:37 -0600

    Add files via upload

--- entry/main.cpp
+++ entry/main.cpp
@@ -35,16 +35,21 @@ int main(){
     testBSTDatasetSmall();
     testBSTDatasetBig();
     testAddEnemies();
-    
-//     Room r;
-//     r.makeRoom(10,10);
-//     std::cout << "made room" << std::endl;
-//     BST tree = BST();
-//     std::cout << "made bst" << std::endl;
-//     r.addEnemies(&tree);
-//     std::cout << "added enemies" << std::endl;
-//     cs225::PNG* unsolved = r.drawRoom();
-//     unsolved->writeToFile("unsolved.png");
-//     std::cout << "drew room" << std::endl;
-     
+    Room r;
+    r.makeRoom(22,22);
+    BST tree = BST("../src/large_twitch_features.csv", 1000);
+    r.addEnemies(&tree);
+    cs225::PNG* unsolved = r.drawRoom();
+    unsolved->writeToFile("unsolved.png");
+    std::cout<<"Walking Weight = 0, 43x43, green.png"<<std::endl;
+    cs225::PNG* solved = r.drawRoomSolution(0);
+    solved->writeToFile("green.png");
+    std::cout<<"Walking Weight = 10, 43x43, yellow.png"<<std::endl;
+    r.setWalkingDistance(10);
+    cs225::PNG* solved2 = r.drawRoomSolution(50);
+    solved2->writeToFile("yellow.png");
+    r.setWalkingDistance(10);
+    std::cout<<"Walking Weight = 20, 43x43, red.png"<<std::endl;
+    cs225::PNG* solved1 = r.drawRoomSolution(125);
+    solved1->writeToFile("red.png");
 }
