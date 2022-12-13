dd949cf88483ddd3dd143f1b12ff34d8e1f2ae2c
hmshah2 <93108170+hmshah2@users.noreply.github.com>
2022-12-12 21:30:32 -0600

    added comments

--- src/bst.cpp
+++ src/bst.cpp
@@ -8,6 +8,9 @@
 #include <chrono>
 #include "bst.h"
 
+//bst contructor
+//takes in a csv datafile by name and an integer representing the size of the bst wanted
+//creates a bst of the top num_enemies number of twitch streamers
 BST::BST(std::string file_name, int num_enemies) {
 
     std::string lines;
@@ -24,6 +27,7 @@ BST::BST(std::string file_name, int num_enemies) {
             }
             if (std::stoi(row[0]) >= 0) {
                 gamers.push_back(std::stoi(row[0]));
+                //inserts the number of streams for each twitch gamer
             }  
         }
     }
@@ -31,18 +35,24 @@ BST::BST(std::string file_name, int num_enemies) {
     file.close();
     sort(gamers.rbegin(), gamers.rend());
     gamers.erase(gamers.begin() + num_enemies, gamers.end());
+    //sorts it from most to least to get the top gamers of size num_enemies
     
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine e(seed);
     std::shuffle(gamers.begin(), gamers.end(), e);
+    //randomly sorts the gamers vector for a more efficient bst contruction
     
     for (int i = 0; i < num_enemies; i++) {
         insert(gamers[i]);
+        //inserts into the bst
     }
 }
 struct BST::Node* BST::getRoot(){
     return root;
 }
+
+//returns a vector representing either a pre, in, or post order traversal of the bst depending on the parameter passed in 0, 1, and 2, respectively
+//used to make the implementation of Room::addEnemies more randomlized
 std::vector<int> BST::getAllNodes(int c){
     if (c == 0) {
         allNodes_one.clear();
@@ -59,6 +69,7 @@ std::vector<int> BST::getAllNodes(int c){
     return allNodes_three;
 }
 
+//inserts a key into the bst (stores each gamer/enemy)
 void BST::insert(const int & key) {
     Node* n = new Node(key);
     if (root == NULL) {
@@ -83,6 +94,7 @@ void BST::insert(const int & key) {
     }
 }
 
+//preorder traversal of the bst
 void BST::preOrder(Node* n) {
     if (n == NULL) {
         return;
@@ -92,6 +104,7 @@ void BST::preOrder(Node* n) {
     preOrder(n->right);
 }
 
+//inorder traversal of the bst
 void BST::inOrder(Node* n) {
     if (n == NULL) {
         return;
@@ -101,6 +114,7 @@ void BST::inOrder(Node* n) {
     inOrder(n->right);
 }
 
+//postorder traversal of the bst
 void BST::postOrder(Node* n) {
     if (n == NULL) {
         return;
