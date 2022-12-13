e4269274faf46cae99e03b4c56d7b8ba2cfde841
hmshah2 <93108170+hmshah2@users.noreply.github.com>
2022-12-12 21:38:08 -0600

    add comments

--- src/bst.h
+++ src/bst.h
@@ -11,6 +11,7 @@
 class BST
 {
   private:
+  //each value in the bst is a node with a key and can access its left and right nodes
     struct Node {
         int key;
         Node* left;
@@ -23,17 +24,39 @@ class BST
     };
 
   public:
+    //bst contructor
+    //takes in a csv datafile by name and an integer representing the size of the bst wanted
+    //creates a bst of the top num_enemies number of twitch streamers
     BST(std::string file_name, int num_enemies);
+  
+    //inserts a key into the bst (stores each gamer/enemy)
     void insert(const int& key);
+    
+    //preorder traversal of the bst
     void preOrder(Node* n);
+  
+    //inorder traversal of the bst
     void inOrder(Node* n);
+  
+    //postorder traversal of the bst
     void postOrder(Node* n);
+  
+    //returns the root
     Node* getRoot();
+    
+    //returns a vector representing either a pre, in, or post order traversal of the bst depending on the parameter passed in 0, 1, and 2, respectively
+    //used to make the implementation of Room::addEnemies more randomlized
     std::vector<int> getAllNodes(int c);
 
   private:
     Node* root = NULL;
+  
+    //vector that stores the preorder traversal for the bst
     std::vector<int> allNodes_one;
+  
+    //vector that stores the preorder traversal for the bst
     std::vector<int> allNodes_two;
+  
+    //vector that stores the preorder traversal for the bst
     std::vector<int> allNodes_three;
 };
